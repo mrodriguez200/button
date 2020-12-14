4b330845dd87f659e7ebb0457d23158ff076194c
Michael Rodriguez <70351660+mrodriguez200@users.noreply.github.com>
2020-12-13 23:17:27 -0500

    button

---
+++ button.js
@@ -0,0 +1,10 @@
+
+  function msg(){  
+    alert("Great Job!"); 
+    changeColor('red'); 
+    el_down.innerHTML = "Background Color changed";  
+   }
+   function changeColor(color) { 
+    document.body.style.background = color; 
+}
+              
